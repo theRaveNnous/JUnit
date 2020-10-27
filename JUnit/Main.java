95c81013a31ae2946ccd61b0e053a1a607e7d30e
u33998@hs-harz.de <u33998@hs-harz.de>
2020-10-27 14:43:12 +0100

    test

--- JUnit/Main.java
+++ JUnit/Main.java
@@ -3,7 +3,8 @@ package JUnit;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("test");
+		System.out.println(args[0]);
+		System.out.println(args[1]);
 	}
 
 }
